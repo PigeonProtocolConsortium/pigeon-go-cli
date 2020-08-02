fb195f72e0ca5058ebddd35f822d15f8d0120373
Rick Carlino <rick.carlino@gmail.com>
2020-08-02 14:10:32 -0500

    Add `pigeon version` helper

--- cmd/version.go
+++ cmd/version.go
@@ -1,7 +1,22 @@
 package cmd
 
 import (
+	"fmt"
 	"pigeon/pigeon"
+
+	"github.com/spf13/cobra"
 )
 
-const x = pigeon.Version
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the version of the Pigeon CLI tool.",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		result := fmt.Sprintf("Pigeon v%s", pigeon.Version)
+		fmt.Println(result)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
